b113fc2fd41337a9da4f8af5a05a949eed807fa6
stefanstefanovic94 <stefan.stef94@gmail.com>
2020-07-19 14:50:12 +0200

    Updates

---
+++ precache-manifest.c5a86593af0f4547787c3237a10df77a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d2b0510ff2ebf438e07bfc5433eba8",
+    "url": "/Blog/index.html"
+  },
+  {
+    "revision": "233d0b91f5c48a2b7a4b",
+    "url": "/Blog/static/css/2.6cc87aa2.chunk.css"
+  },
+  {
+    "revision": "7dedce9b0823daaa19b6",
+    "url": "/Blog/static/css/main.1f7969ef.chunk.css"
+  },
+  {
+    "revision": "233d0b91f5c48a2b7a4b",
+    "url": "/Blog/static/js/2.3f1b7e61.chunk.js"
+  },
+  {
+    "revision": "35937c55f4ccbb69aaefc08b51c916a0",
+    "url": "/Blog/static/js/2.3f1b7e61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dedce9b0823daaa19b6",
+    "url": "/Blog/static/js/main.89e869ef.chunk.js"
+  },
+  {
+    "revision": "4fd20db89e7a2dd269fa",
+    "url": "/Blog/static/js/runtime-main.b17175f3.js"
+  }
+]);
